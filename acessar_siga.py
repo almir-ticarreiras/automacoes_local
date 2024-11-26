4bd6dd9f70bf8ab12413ba78bee97fa01ea49af5
Almir Ferreira <ticarreiras@gmail.com>
2024-11-26 17:06:47 -0300

    acessar o siga e gerar relatório razao

--- acessar_siga.py
+++ acessar_siga.py
@@ -2,8 +2,10 @@ import pyautogui
 import time
 
 pyautogui.PAUSE = 0.5
-tempo_de_espera = 3
+tempo_de_espera = 1
 tempo_transicao = 1
+data_inicio = "01102024"
+data_fim = "31102024"
 
 pyautogui.press("win")
 pyautogui.write("chrome")
@@ -39,3 +41,55 @@ pyautogui.scroll(-100)
 #Clica no menu Razão
 pyautogui.moveTo(x=87, y=633, duration=tempo_transicao)
 pyautogui.click()
+
+#Clica na opção de selecionar Localizada
+pyautogui.moveTo(x=548, y=328, duration=tempo_transicao)
+pyautogui.click();
+
+#Seleciona a Opção "ADM - ITUPEVA - SP"
+pyautogui.moveTo(x=335, y=420, duration=tempo_de_espera)
+pyautogui.click()
+
+#Clica na opção de selecionar Centro de Custo
+pyautogui.moveTo(x=323, y=393, duration=tempo_de_espera)
+pyautogui.click()
+
+#Seleciona o Opção "ADM - ITUPEVA - SP"
+pyautogui.moveTo(x=350, y=492, duration=tempo_de_espera)
+pyautogui.click()
+
+#Clica na opção para selecionar o Tipo de saída
+pyautogui.moveTo(x=300, y=593, duration=tempo_de_espera)
+pyautogui.click()
+
+#Seleciona a opção "Excel"
+pyautogui.moveTo(x=250, y=529, duration=tempo_de_espera)
+pyautogui.click()
+
+#Clica no campo de data inicio
+pyautogui.moveTo(x=588, y=260, duration=tempo_de_espera)
+pyautogui.click()
+
+#Limpa a data início pré selecionada e informa uma nova
+pyautogui.press("backspace", presses=8)
+pyautogui.write(data_inicio)
+
+#Clica no campo de data fim
+pyautogui.moveTo(x=734, y=261, duration=tempo_de_espera)
+pyautogui.click()
+
+#Limpa a data fim pré selecionada e informa uma nova
+pyautogui.press("backspace", presses=8)
+pyautogui.write(data_fim)
+
+#Clica em gerar relatório
+pyautogui.moveTo(x=282, y=674, duration=tempo_de_espera)
+pyautogui.click()
+
+# Atalho para abrir o Explorador de Arquivos (Windows + E)
+pyautogui.hotkey('win', 'e')
+time.sleep(1)
+
+# Digita o caminho da pasta Downloads
+pyautogui.typewrite('C:\\Users\\Usuario\\Downloads')  # Altere "SeuUsuario" pelo nome do usuário do seu sistema
+pyautogui.press('enter')
